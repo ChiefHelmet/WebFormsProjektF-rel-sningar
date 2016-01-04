b10b51d5d5218e16f32578998ce4737d8157e812
DavidJarl <kylochfrys@hotmail.com>
2016-01-04 15:14:13 +0100

    Still not comprehended code concerning databases

---
+++ 006 övning2 databas/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_006_övning2_databas.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button runat="server" id="buttonOne" OnClick="buttonOne_OnClick"/>
+        <asp:DropDownList runat="server" id="DropDownList1" AutoPostBack="True"/>
+        <asp:Label runat="server" ID="Label1" Text="" Visible="False"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 006 övning2 databas/Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _006_övning2_databas
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new sqlConnection("Data source=localhost:initial Catalog=AdventureWorks2012;User ID=myuser; Password=mypassword;Integrated Security = True");
+            string sqlQuery = "SELECT * FROM Production.ProductCategory";
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+            SqlDataReader oreader;
+            try
+            {
+                ListItem newItem = new ListItem();
+                newItem.Text = "Select";
+                newItem.Value = "0";
+                DropDownList1.Items.Add(newItem);
+                con.Open();
+                oreader = cmd.ExecuteReader();
+                while (oreader.Read())
+                {
+                    ListItem listItem1 = new ListItem();
+                    listItem1.Text = oreader["Name"].ToString();
+                    listItem1.Value = oreader["ProductCategoryID"].ToString();
+                    DropDownList1.Items.Add(listItem1);
+                }
+                oreader.Close();
+                oreader.Dispose();
+                con.Close();
+                con.Dispose();
+                cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = ex.Message;
+                Label1.Visible = true;
+            }
+        }
+
+        //protected void buttonOne_OnClick(object sender, EventArgs e)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
